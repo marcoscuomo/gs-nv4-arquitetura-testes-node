ec299b867e06fd7f15e6852eb114628974292bf6
Marcos Souza <bytecore@gmail.com>
2021-02-21 01:32:58 -0300

    Teste de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,21 +1,39 @@
+import AppError from '@shared/erros/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointmentService', () => {
-    it('should be able to create a new appointment', async () => {
+    // it('should be able to create a new appointment', async () => {
+    //     const fakeAppointmentRepository = new FakeAppointmentRepository();
+    //     const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+
+    //     const appointment = await createAppointment.execute({
+    //         date: new Date(),
+    //         provider_id: '2332'
+    //     });
+
+    //     expect(appointment).toHaveProperty('id');
+    //     expect(appointment.provider_id).toBe('2332');
+    // });
+
+    it('should not be able to create two appointments on the same time', async () => {
+
         const fakeAppointmentRepository = new FakeAppointmentRepository();
         const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
 
-        const appointment = await createAppointment.execute({
-            date: new Date(),
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
             provider_id: '2332'
         });
 
-        expect(appointment).toHaveProperty('id');
-        expect((await appointment).provider_id).toBe('2332');
-    });
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '2332'
+            })
+        ).rejects.toBeInstanceOf(AppError);
 
-    // it('should not be able to create two appointments on the same time', () => {
-    //     expect(1+2).toBe(3);
-    // });
+    });
 });
