048a38252f436e23b77e09b90e9cd535f8105016
Marcos Souza <bytecore@gmail.com>
2021-02-21 01:04:36 -0300

    Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('sim two numbers', () => {
-    expect(1+2).toBe(3);
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointmentService', () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentRepository = new FakeAppointmentRepository();
+        const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '2332'
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect((await appointment).provider_id).toBe('2332');
+    });
+
+    // it('should not be able to create two appointments on the same time', () => {
+    //     expect(1+2).toBe(3);
+    // });
 });
